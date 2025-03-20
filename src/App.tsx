d37173f0a23cd65ab9abc361e43627aa60c28902
Ali-Zaraket <alizaraket600@gmail.com>
2025-03-21 00:35:42 +0200

    fix ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from "react";
+import { Suspense } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home";
 import NotFound from "./pages/not-found";
